aa785e2434b38cfccea0386bd10544c73769ab1c
zzaeun <leeje57575@gmail.com>
2025-04-27 19:26:55 +0900

    Initial Commit

---
+++ Foundation_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foundation_Practice
+//
+//  Created by 이정은 on 4/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foundation_Practice/Foundation_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foundation_PracticeApp.swift
+//  Foundation_Practice
+//
+//  Created by 이정은 on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Foundation_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
